339508dbf2d0ecbb377b57d7b0f6232a8166ef50
wilsonzheng1x <58754942+wilsonzheng1x@users.noreply.github.com>
2020-01-19 21:04:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,69 +6,7 @@ var placeholder_2 = " mph";
 
 
 
-function forecast(val) {
-        var now = new Date();
-        var val2 = document.querySelector('input[name="mi"]:checked').value;
-        if (val2 == "metric") {
-                placeholder_1 = " °C";
-                placeholder_2 = " kph";
-        } else {
-                placeholder_1 = " °F";
-                placeholder_2 = " mph";
-        }
-        fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + val + "&appid=d4165ac5f232bf585af32a4ba1d2e0a6&units=" + val2 + "&cnt=9")
-        .then(response => response.json())
-        .then(data => {
-                var number = 0;
-                for ( ; number < 9; number++) {
-                        document.getElementById("time" + (number + 2).toString()).innerHTML=TimeChange(data.list[number].dt + timezone_);
-                        document.getElementById("temp" + (number + 2).toString()).innerHTML=data.list[number].main.temp + placeholder_1;
-                        document.getElementById("wind" + (number + 2).toString()).innerHTML=data.list[number].wind.speed + placeholder_2;
-                        document.getElementById("precip" + (number + 2).toString()).innerHTML=data.list[number].weather[0].description;
-                        document.getElementById("image" + (number + 2).toString()).src="https://openweathermap.org/img/wn/" + data.list[number].weather[0].icon + "@2x.png";
-                        }
-                
-                })
-
-}
-        
-
-function TimeChange(org) {
-	var remainder = org%86400;
-	var hours = Math.floor(remainder/3600);
-	var minutes = Math.floor((org%3600)/60);
-	var returner = "";
-	var AM = "AM";
-	if (hours > 12) {
-		returner = (hours - 12).toString();
-		AM = "PM";
-	} else if (hours == 12) {
-                AM = "PM";
-                returner = hours.toString();
-        } else if (hours == 0) {
-		returner = "12";
-	} else {
-		returner = hours.toString();
-	}
-        
-	if (minutes < 10) {
-		returner += ":0" + minutes.toString();
-	} else {
-		returner += ":" + minutes.toString();
-	}
-	return returner + " " + AM;
-}
-
-function TimePrint(h, m) {
-	var returner = "";
-	var AM  = "AM";
-        if (h < 0) {
-                h = h + 24;
-        } else if (h > 23) {
-                h = h - 24;
-        }
-	if (h > 12) {
-		returner = (h - 12).toString();
+f
 		AM = "PM";
 	} else if (h == 12) {
                 AM = "PM";
